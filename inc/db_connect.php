b7a7df03c3932185060dedfb8b34e433889ffac6
wkwyatt <wkwyatt1@gmail.com>
2016-01-18 09:17:48 -0500

    mysql update

--- inc/db_connect.php
+++ inc/db_connect.php
@@ -1,13 +1,17 @@
 <?php
 
-	$link = mysql_connect('127.0.0.1', 'college-hoops', 'x');
+	$link = mysqli_connect('127.0.0.1', 'college-hoops', 'x');
 	if (!$link) {
 	    die('Not connected : ' . mysql_error());
 	}
 	// make phpland the current db
-	$db_selected = mysql_select_db('basketball', $link);
+	$db_selected = mysqli_select_db( $link, 'basketball');
 	if (!$db_selected) {
-	    die ('Can\'t use phpland : ' . mysql_error());
+	    die ('Can\'t use phpland : ' . mysqli_error());
 	}
 
+	// require_once 'meekrodb.class.php';
+	// DB::$user = 'college-hoops';
+	// DB::$password = 'x';
+	// DB::$dbName = 'basketball';
 ?>
